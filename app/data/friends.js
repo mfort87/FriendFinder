4262c15d5ae4e0d92400f41776aa86fa8abdc29e
Matt Fort <mfort8487@gmail.com>
2018-09-06 18:41:58 -0500

    added friends.js code

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,24 +1,162 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var path = require('path');
-
-// Configure the Express application
-var app = express();
-var PORT = process.env.PORT;
-
-// Expose the public directory to access CSS files
-app.use(express.static(path.join(__dirname, './app/public')));
-
-// Add middleware for parsing incoming request bodies
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.text());
-
-// Add the application routes
-require(path.join(__dirname, './app/routing/apiRoutes'))(app);
-require(path.join(__dirname, './app/routing/htmlRoutes'))(app);
-
-// Start listening on PORT
-app.listen(PORT, function() {
-  console.log('Friend Finder app is listening on PORT: ' + PORT);
-});
+var friendsArray = [
+	{
+		name: 'Skeeter',
+		photo: 'https://pre00.deviantart.net/7d73/th/pre/i/2014/131/3/b/skeeter__doug__by_4and4-d7i1ibm.jpg',
+		scores: [
+			'4',
+			'3',
+			'2',
+			'2',
+			'5',
+			'1',
+			'2',
+			'3',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Doug',
+		photo: 'https://timeentertainment.files.wordpress.com/2011/07/09_top10cartoonthemesongs1.jpg?w=480&h=320&crop=1',
+		scores: [
+			'3',
+			'2',
+			'4',
+			'4',
+			'5',
+			'2',
+			'3',
+			'3',
+			'2',
+			'5'
+		]
+	},
+	{
+		name: 'Patty',
+		photo: 'https://carboncostume.com/wordpress/wp-content/uploads/2017/11/Patti-Mayonnaise.jpg',
+		scores: [
+			'2',
+			'3',
+			'4',
+			'4',
+			'1',
+			'1',
+			'2',
+			'5',
+			'1',
+			'1'
+		]
+	},
+	{
+		name: 'Roger',
+		photo: 'https://img00.deviantart.net/8ad2/i/2012/171/f/a/roger_klotz_and_stinky_by_luthien_black-d2oa0qd.jpg',
+		scores: [
+			'5',
+			'3',
+			'1',
+			'2',
+			'2',
+			'2',
+      '3',
+			'1',
+			'5',
+			'5'
+		]
+	},
+	{
+		name: 'Judy',
+		photo: 'https://img.sharetv.com/shows/characters/large/doug.judith_judy_anastasia_funnie.jpg',
+		scores: [
+			'1',
+			'5',
+			'4',
+			'3',
+			'3',
+			'3',
+			'4',
+			'4',
+			'2',
+			'2'
+		]
+	},
+	{
+		name: 'Mrs. Piggy',
+		photo: 'http://img.mindbodygreen.com/image/upload/c_limit,w_738,f_auto/ftr/Miss-Piggy-large-tonya-312.jpg',
+		scores: [
+			'1',
+			'1',
+			'5',
+			'1',
+			'3',
+			'2',
+			'5',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Quailman',
+		photo: 'https://vignette.wikia.nocookie.net/doug/images/8/8c/581278-image_2_quailman_large.jpeg/revision/latest?cb=20111006235840',
+		scores: [
+			'5',
+			'4',
+			'4',
+			'2',
+			'4',
+			'4',
+			'3',
+			'2',
+			'3',
+			'3'
+		]
+	},
+	{
+		name: 'Beebe',
+		photo: 'https://vignette.wikia.nocookie.net/doug/images/e/e3/Beebe_Bluff.png/revision/latest?cb=20180516112344',
+		scores: [
+			'5',
+			'2',
+			'3',
+			'1',
+			'5',
+			'1',
+			'3',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Porkchop',
+		photo: 'https://vignette.wikia.nocookie.net/doug/images/0/08/Porkchop.png/revision/latest?cb=20180531191333',
+		scores: [
+			'5',
+			'3',
+			'3',
+			'2',
+			'3',
+			'3',
+			'3',
+			'5',
+			'5',
+			'5'
+		]
+	},
+	{
+		name: 'Connie',
+		photo: 'https://statici.behindthevoiceactors.com/behindthevoiceactors/_img/chars/connie-benge-dougs-1st-movie-3.9.jpg',
+		scores: [
+			'2',
+			'5',
+			'5',
+			'3',
+			'3',
+			'2',
+			'3',
+			'5',
+			'3',
+			'1'
+		]
+	}
+];
